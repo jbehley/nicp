d450429c862386c0fb219e5cc71a93f59e1090e8
Jacopo Serafin <jacopo.serafin@gmail.com>
2016-11-04 17:15:51 +0100

    Added gl_helpers.

---
+++ src/nicp_gl_helpers/draw_attributes.h
@@ -0,0 +1,8 @@
+#pragma once
+
+typedef int DrawAttributesType;
+
+#define ATTRIBUTE_SHOW     0x1  // default, shows an object
+#define ATTRIBUTE_SELECTED 0x2  // policy for a selected object
+#define ATTRIBUTE_ONLY     0x4  // draw only attributes 
+
